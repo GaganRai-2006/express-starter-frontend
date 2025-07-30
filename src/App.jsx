81e5a4b3b3ae91e4dab5d54eaf5d5e84c4ae664a
gagan <raigagan084@gmail.com>
2025-07-30 11:15:02 +0530

    added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Home from './pages/Home'
 import Signup from './pages/Auth/Signup'
 import Login from './pages/Auth/Login'
+import NotFound from './pages/NotFound'
 
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/auth/signup" element={< Signup/>} />
         <Route path="/auth/login" element={< Login/>}/>
+
+        <Route path='*' element={<NotFound/>}/>
+
       </Routes>
     </>
   )
